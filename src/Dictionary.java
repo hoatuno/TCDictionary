e3a5730f75a787a6531975d4d3a512cc3ae346c6
hoatuno <19020066@vnu.edu.vn>
2020-09-29 23:03:43 +0700

    ccc

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -2,72 +2,64 @@ import java.io.*;
 import java.util.*;
 
 public class Dictionary {
-   List<Word> wordList = new ArrayList<>();
-     class DictionaryManagement {
-        Dictionary dictionary = new Dictionary();
+    public List<Word> wordList = new ArrayList<>();
 
-        public void insertFromCommandLine() {
-
-            }
-        }
-    }
-    class DictionaryCommandline {
-
-        Dictionary dictionary = new Dictionary();
-
-        public void showAllWords() {
-            for(int i=0; i< dictionary.wordList.size(); i++) {
-                String wd = dictionary.wordList.get(i).getWord();
-                String def = dictionary.wordList.get(i).getDef();
-                System.out.println(i+1 + "      | " + "wd" + "            | " + "def");
-            }
-        }
-    }
-    public static void main(String[] args) {
-        Dictionary dictionary = new Dictionary();
-
+    public void insertFromCommandLine() {
         Scanner scanner = new Scanner(System.in);
         int numofWords = scanner.nextInt();
         scanner.nextLine();
         while (numofWords > 0) {
-
             String word = scanner.nextLine();
             String def = scanner.nextLine();
             Word wordObj = new Word(word, def);
-            dictionary.wordList.add(wordObj);
+            wordList.add(wordObj);
             numofWords--;
-
-        //dcl.showAllWords();
+        }
     }
+
+    public void showAllWords() {
+        for (int i = 0; i < wordList.size(); i++) {
+            String wd = wordList.get(i).getWord();
+            String def = wordList.get(i).getDef();
+            System.out.println(i + 1 + "      | " + wd + "            | " + def);
+        }
+    }
+
+    public static void main(String[] args) {
+        Dictionary dict = new Dictionary();
+        dict.insertFromCommandLine();
+        System.out.println(dict.wordList.size());
+        dict.showAllWords();
+    }
+
+    public void dictionaryBasic() {
+
+
+    }
+    public  void insert(String Filename) throws IOException {
+        // Đọc dữ liệu
+        File file = new File(Filename);
+        FileReader fis = new FileReader(file);
+        BufferedReader br = new BufferedReader(fis);
+        String line;
+        int i = 0;
+        while ((line = br.readLine()) != null) {
+            String[] parts = line.split("<html>");
+            String word = parts[i];
+            String definition = "<html>" + parts[i + 1];
+            System.out.println(word);
+            System.out.println("==> " + definition);
+            Word wordObj = new Word(word, definition);
+            wordList.add(wordObj);
+            i++;
+            if (i > 5) break;
+
+        }
+    }
+
+
 
 }
-
-class Word {
-    private String word;
-    private String def;
-
-    public Word(String word, String def) {
-        this.word = word;
-        this.def = def;
-    }
-
-    public String getWord() {
-        return word;
-    }
-
-    public void setWord(String word) {
-        this.word = word;
-    }
-
-    public String getDef() {
-        return def;
-    }
-
-    public void setDef(String def) {
-        this.def = def;
-    }
-}
-
 
 
 
--- src/insertFromFIle.java
+++ src/insertFromFIle.java
@@ -1,29 +1,12 @@
 import java.io.*;
+
 // this class takes responsibility for reading/writing from/to files
-public class FileReader {
+public class insertFromFIle {
 
-
-    public static Dictionary FileReader (Dictionary dictionary){
-          // Đọc dữ liệu
-          FileReader fis = new FileReader("data.txt");
-          BufferedReader br = new BufferedReader(fis);
-
-          String line;
-          int i = 0;
-          while ((line = br.readLine()) != null) {
-              String[] parts = line.split("<html>");
-              String word = parts[0];
-              String definition = "<html>" + parts[1];
-              System.out.println(word);
-              System.out.println("==> " + definition);
-              Word wordObj = new Word(word, definition);
-              dictionary.wordList.add(wordObj);
-              i++;
-              if (i > 5) break;
-          }
-      }
-
-
+    public static void main(String[] args) {
+        Dictionary mydict = new Dictionary();
+        mydict.insert("data.txt");
+    }
 
 
 }
